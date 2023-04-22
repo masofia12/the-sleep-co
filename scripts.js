49101f28e32f83ff9a680facdea94dfadcb8ac67
masofia12 <129247302+masofia12@users.noreply.github.com>
2023-04-22 15:15:28 +0900

    java

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $('.hamburger-button').click(function(){
+        
+        $(this).toggleClass('active');
+        $('.mobile-menu').fadeToggle();
+
+      });
+    
+    
+      // animate on scroll activate
+      AOS.init({
+        duration: 500
+      });
+
+});
